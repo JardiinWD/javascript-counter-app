8098b08fb0bcbc653f3298b98061a5697201dedb
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-26 20:06:42 +0200

    Aggiunto eventi ai miei pulsanti per funzionamento app

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,22 @@ console.log(add); /* Lo verifico in console */
 /* #1.3 - Prendo il mio button con classe reset  */
 const resetCount = document.querySelector(".reset")
 console.log(resetCount); /* Lo verifico in console */
+
+/* #1.4 - Scateno un evento sul pulsante Add che, al click dovrà cambiare valore (in positivo) allo span .count */
+add.addEventListener("click", () => {
+    /* console.log("Stai cliccando su Add"); */ // Verifico in console
+    count.innerHTML++; // Questo incrementa lo span di 1
+})
+
+/* #1.5 - Scateno un evento sul pulsante substract che, al click dovrà cambiare valore (in negativo) allo span .count */
+substract.addEventListener("click", () => {
+    /* console.log("Stai cliccando su substract"); */ // Verifico in console
+    count.innerHTML--; // Questo decrementa lo span di 1
+})
+
+/* #1.6 - Scateno un evento sul pulsante reset che, al click dovrà cambiare resettare il valore allo span .count */
+resetCount.addEventListener("click", () => {
+    /* console.log("Stai cliccando su reset"); */ // Verifico in console
+    count.innerHTML = 0; // Questo riporta il valore a 0
+})
+
