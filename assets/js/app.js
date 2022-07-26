27a8b6a7f42d1d9d558b6746ee2f94221fde6d1f
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-26 19:59:47 +0200

    Individuato e selezionato classi di cui necessito in js

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+/* # -> Commenti seguiti da asterisco sono i passaggi necessari per il corretto funzionamento app */
+
+/* #1 - Prendo il mio span con classe count  */
+const count = document.querySelector(".count")
+console.log(count); /* Lo verifico in console */
+
+/* #1.1 - Prendo il mio button con classe substract  */
+const substract = document.querySelector(".substract")
+console.log(substract); /* Lo verifico in console */
+
+/* #1.2 - Prendo il mio button con classe add  */
+const add = document.querySelector(".add")
+console.log(add); /* Lo verifico in console */
+
+/* #1.3 - Prendo il mio button con classe reset  */
+const resetCount = document.querySelector(".reset")
+console.log(resetCount); /* Lo verifico in console */
